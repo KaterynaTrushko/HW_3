5e8b8d1743502139d6771dd575fd3962223a52b3
Kateryna Trushko <trushkokateryna@gmail.com>
2024-04-28 18:15:26 +0100

    add queue

---
+++ Queue/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+    {
+    using System;
+    using System.Collections.Generic;
+
+    public class CustomQueue<T>
+        {
+        private List<T> queue = new List<T>();
+
+        public void Enqueue(T item)
+            {
+            queue.Add(item);
+            }
+
+        public T Dequeue()
+            {
+            if (queue.Count == 0)
+                {
+                throw new InvalidOperationException("Queue is empty");
+                }
+
+            T item = queue[0];
+            queue.RemoveAt(0);
+            return item;
+            }
+
+        public void Clear()
+            {
+            queue.Clear();
+            }
+
+        public bool Contains(T item)
+            {
+            return queue.Contains(item);
+            }
+
+        public T Peek()
+            {
+            if (queue.Count == 0)
+                {
+                throw new InvalidOperationException("Queue is empty");
+                }
+
+            return queue[0];
+            }
+
+        public T[] ToArray()
+            {
+            return queue.ToArray();
+            }
+
+        public int Count
+            {
+            get { return queue.Count; }
+            }
+        }
+
+    }
---
+++ Queue/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Queue
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            var queue = new CustomQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(1);
+            queue.Contains(2);
+            int a = queue.Count;
+            queue.Clear();
+            }
+        }
+    }
